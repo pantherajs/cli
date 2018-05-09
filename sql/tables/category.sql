f95264f69d2069ed373173844632642eca239ff3
Connor <wiseman.connor@gmail.com>
2018-05-09 11:46:40 -0600

    category

---
+++ sql/tables/category.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS category;
+
+CREATE TABLE IF NOT EXISTS category (
+  id       SERIAL                   NOT NULL,
+  name     CHARACTER VARYING(64)    NOT NULL,
+  sort_key INTEGER                  NOT NULL DEFAULT 0,
+  created  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  modified TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  CONSTRAINT category_primary_key PRIMARY KEY (id),
+  CONSTRAINT category_unique_name UNIQUE (name)
+);
+
+CREATE TRIGGER update_category_modified BEFORE UPDATE ON category
+  FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+GRANT USAGE, SELECT ON SEQUENCE category_id_seq TO %I;
+GRANT SELECT, INSERT, UPDATE, DELETE ON category TO %I;
---
+++ test/tasks/sql/tables/category.spec.js
@@ -0,0 +1,24 @@
+/**
+ * @file test/tasks/sql/tables/category.spec.js
+ */
+'use strict';
+
+const test       = require('ava');
+const proxyquire = require('proxyquire');
+const sinon      = require('sinon');
+
+const stubs = {
+  '../../../utils/sql-task': sinon.stub().resolves()
+};
+
+const category = proxyquire('../../../../bin/tasks/sql/tables/category', stubs);
+
+test('should create `category` table', async t => {
+  const context = {
+    env: {
+      PANTHERA_API_USER: 'user'
+    }
+  };
+
+  await t.notThrows(() => category.task(context));
+});
