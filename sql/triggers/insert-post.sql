c7538678e9b1d0cf3869ecd20b2c9460b031f573
Connor <wiseman.connor@gmail.com>
2018-05-28 10:37:44 -0600

    insert_post() trigger

---
+++ sql/triggers/insert-post.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION insert_post()
+RETURNS TRIGGER AS
+$insert_post$
+BEGIN
+  UPDATE topic_statistics_mat SET
+    expiry = NEW.created
+  WHERE topic_id = NEW.topic_id;
+
+  UPDATE forum_statistics_mat SET
+    expiry = NEW.created
+  WHERE forum_id IN ((
+    SELECT ancestor_forums((
+      SELECT
+        topic.forum_id
+      FROM topic
+      WHERE topic.id = NEW.topic_id
+    ))
+  ));
+
+  RETURN NEW;
+END;
+$insert_post$
+  VOLATILE
+  SECURITY DEFINER
+  LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_post AFTER INSERT ON post
+  FOR EACH ROW EXECUTE PROCEDURE insert_post();
---
+++ test/tasks/sql/triggers/insert-post.spec.js
@@ -0,0 +1,18 @@
+/**
+ * @file test/tasks/sql/triggers/insert-post.spec.js
+ */
+'use strict';
+
+const test       = require('ava');
+const proxyquire = require('proxyquire');
+const sinon      = require('sinon');
+
+const stubs = {
+  '../../../utils/sql-task': sinon.stub().resolves()
+};
+
+const insert = proxyquire('../../../../bin/tasks/sql/triggers/insert-post', stubs);
+
+test('should create `insert_post` trigger', async t => {
+  await t.notThrows(() => insert.task({}));
+});
