97d35f1019ad8b817b76978c8df8a4cea8b68d85
Connor <wiseman.connor@gmail.com>
2018-05-28 10:33:03 -0600

    refresh_forum_statistics_mat()

---
+++ sql/functions/refresh-forum-statistics-mat.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION refresh_forum_statistics_mat(target_id INTEGER)
+RETURNS forum_statistics_mat AS
+$refresh_forum_statistics_mat$
+  WITH cte AS (
+    SELECT
+      COALESCE(
+        COUNT(*) FILTER (WHERE topic.created <= CURRENT_TIMESTAMP),
+        0
+      ) AS num_topics,
+      COALESCE(
+        SUM(topic_statistics.num_posts),
+        0
+      ) AS num_posts,
+      MAX(topic_statistics.recent_post_id) AS recent_post_id,
+      COALESCE(
+        MIN(topic.created) FILTER (WHERE CURRENT_TIMESTAMP < topic.created),
+        'infinity'::TIMESTAMPTZ
+      ) AS expiry
+    FROM forum
+    LEFT JOIN topic
+      ON topic.forum_id = forum.id
+    INNER JOIN topic_statistics
+      ON topic_statistics.topic_id = topic.id
+    WHERE forum.id IN (SELECT descendant_forums(target_id))
+  )
+  UPDATE forum_statistics_mat SET
+    num_topics      = cte.num_topics,
+    num_posts       = cte.num_posts,
+    recent_topic_id = topic.id,
+    recent_post_id  = cte.recent_post_id,
+    expiry          = cte.expiry
+  FROM cte
+  LEFT JOIN post
+    ON cte.recent_post_id = post.id
+  LEFT JOIN topic
+    ON post.topic_id = topic.id
+  WHERE forum_statistics_mat.forum_id = target_id
+  RETURNING forum_statistics_mat.*;
+$refresh_forum_statistics_mat$
+  VOLATILE
+  SECURITY DEFINER
+  LANGUAGE sql;
