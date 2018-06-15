9b24f5cd2c5dcfde33997b41f31d2f939b5eb431
Connor <wiseman.connor@gmail.com>
2018-06-15 11:58:45 -0600

    update_category_permission trigger

---
+++ sql/triggers/update-category-permission.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION update_category_permission()
+RETURNS TRIGGER AS
+$update_category_permission$
+BEGIN
+  IF (OLD.can_view <> NEW.can_view) THEN
+    UPDATE category_viewable_mat SET
+      expiry = NEW.modified
+    WHERE category_id = NEW.id
+      AND NEW.modified <= expiry;
+  END IF;
+
+  RETURN NEW;
+END;
+$update_category_permission$
+  VOLATILE
+  SECURITY DEFINER
+  LANGUAGE plpgsql;
+
+CREATE TRIGGER update_category_permission AFTER UPDATE ON category_permission
+  FOR EACH ROW EXECUTE PROCEDURE update_category_permission();
