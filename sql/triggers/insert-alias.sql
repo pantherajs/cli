3bf18a36a86eeeaa5cee7c8b60d3946e1e9051ec
Connor <wiseman.connor@gmail.com>
2018-06-15 11:46:42 -0600

    category_viewable_mat insert

--- sql/triggers/insert-alias.sql
+++ sql/triggers/insert-alias.sql
@@ -4,6 +4,10 @@ $insert_alias$
 BEGIN
   INSERT INTO alias_statistics_mat (alias_id) VALUES (NEW.id);
 
+  UPDATE category_viewable_mat SET
+    expiry = NEW.created
+  WHERE category_viewable_mat.account_id = NEW.account_id;
+
   RETURN NEW;
 END;
 $insert_alias$
