ae7d29ae3a96bd3e88a98705d0ae564cc36ce340
Connor <wiseman.connor@gmail.com>
2018-06-30 09:20:29 -0600

    update_role() trigger

---
+++ sql/triggers/update-role.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION update_role()
+RETURNS TRIGGER AS
+$update_role$
+BEGIN
+  IF (OLD.name IN ('Administrator', 'Member', 'Character', 'Guest')) THEN
+    RAISE EXCEPTION 'cannot update default role % with id %', NEW.name, NEW.id;
+  END IF;
+
+  RETURN NEW;
+END;
+$update_role$
+  IMMUTABLE
+  SECURITY DEFINER
+  LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_default_row_update BEFORE UPDATE ON role
+  FOR EACH ROW EXECUTE PROCEDURE update_role();
