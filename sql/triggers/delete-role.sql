506b73c1e7badd0c46ef68dd7449cdf382500ea2
Connor <wiseman.connor@gmail.com>
2018-06-30 09:19:55 -0600

    delete_role() trigger

---
+++ sql/triggers/delete-role.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION delete_role()
+RETURNS TRIGGER AS
+$delete_role$
+BEGIN
+  IF (OLD.name IN ('Administrator', 'Member', 'Character', 'Guest')) THEN
+    RAISE EXCEPTION 'cannot delete default role % with id %', OLD.name, OLD.id;
+  END IF;
+
+  RETURN OLD;
+END;
+$delete_role$
+  IMMUTABLE
+  SECURITY DEFINER
+  LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_default_row_delete BEFORE DELETE ON role
+  FOR EACH ROW EXECUTE PROCEDURE delete_role();
