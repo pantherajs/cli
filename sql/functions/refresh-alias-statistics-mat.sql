0038b5b091fb020188cfc26e3e5698a8abf63f8a
Connor <wiseman.connor@gmail.com>
2018-07-07 14:12:36 -0600

    Query optimization: 7s -> 16ms

--- sql/functions/refresh-alias-statistics-mat.sql
+++ sql/functions/refresh-alias-statistics-mat.sql
@@ -1,35 +1,36 @@
 CREATE OR REPLACE FUNCTION refresh_alias_statistics_mat(target_id INTEGER)
 RETURNS alias_statistics_mat AS
 $refresh_alias_statistics_mat$
-  WITH cte AS (
+  WITH topic AS (
     SELECT
       COALESCE(
-        COUNT(DISTINCT topic.*) FILTER (WHERE topic.created <= CURRENT_TIMESTAMP),
+        COUNT(topic.*) FILTER (WHERE topic.created <= CURRENT_TIMESTAMP),
         0
-      ) AS num_topics,
+      ) AS num_topics
+    FROM topic
+    WHERE topic.author_alias_id = target_id
+  ), post AS (
+    SELECT
       COALESCE(
-        COUNT(DISTINCT post.*) FILTER (WHERE post.created <= CURRENT_TIMESTAMP),
+        COUNT(post.*) FILTER (WHERE post.created <= CURRENT_TIMESTAMP),
         0
-      ) AS num_posts,
+      )            AS num_posts,
       MAX(post.id) AS recent_post_id,
       COALESCE(
         MIN(post.created) FILTER (WHERE CURRENT_TIMESTAMP < post.created),
         'infinity'::TIMESTAMPTZ
-      ) AS expiry
-    FROM alias
-    LEFT JOIN topic
-      ON alias.id = topic.author_alias_id
-    LEFT JOIN post
-      ON alias.id = post.author_alias_id
-    WHERE alias.id = target_id
+      )            AS expiry
+    FROM post
+    WHERE post.author_alias_id = target_id
   )
   UPDATE alias_statistics_mat SET
-    num_topics     = cte.num_topics,
-    num_posts      = cte.num_posts,
-    recent_post_id = cte.recent_post_id,
-    expiry         = cte.expiry
-  FROM cte
-  WHERE alias_id = target_id
+    num_topics     = topic.num_topics,
+    num_posts      = post.num_posts,
+    recent_post_id = post.recent_post_id,
+    expiry         = post.expiry
+  FROM topic
+  CROSS JOIN post
+  WHERE alias_statistics_mat.alias_id = target_id
   RETURNING alias_statistics_mat.*;
 $refresh_alias_statistics_mat$
   VOLATILE
